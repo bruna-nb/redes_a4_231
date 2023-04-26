cf847f9080109c7b0be21c79a7cc9d41ed149ccb
Bruna Neto Bitencourt <brunaneto.b@gmail.com>
2023-04-26 00:03:27 -0300

    task: Implementado TCPserver.py

---
+++ TCPserver.py
@@ -0,0 +1,27 @@
+import socket
+
+def get_input(default, message):
+    user_input = input(message)
+    if user_input == "":
+        return default
+    return user_input
+
+host = get_input("localhost", "Host/IP [Default: localhost]: ")
+port = int(get_input(8080, "Port [Default: 8080]: "))
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((host, port))
+server_socket.listen()
+
+print("<<< Socket created >>>")
+print("<<< Socket bind complete >>>")
+
+while True:
+    client_socket, client_address = server_socket.accept()
+    print(f"<<< Accepted connection from {client_address[0]}:{client_address[1]} >>>")
+    while True:
+        message = client_socket.recv(1024).decode()
+        if not message:
+            break
+        print(f"Message [{client_address[0]}:{client_address[1]}]: {message}")
+    client_socket.close()
